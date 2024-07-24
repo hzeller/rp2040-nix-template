6bd11f5278fcef957f71e30be517dfa73c50aec6
Henner Zeller <h.zeller@acm.org>
2024-07-24 09:22:24 -0700

    For now: override until the overridable submodule option is available.

--- shell.nix
+++ shell.nix
@@ -1,24 +1,31 @@
-# Until
-# https://github.com/NixOS/nixpkgs/pull/321786 or
-# https://github.com/NixOS/nixpkgs/pull/260566 is merged, do this
-# manually.
-#{ pkgs ? import <nixpkgs> {} }:
-{ pkgs ? import /home/hzeller/src/my/nixpkgs {} }:
+{ pkgs ? import <nixpkgs> {} }:
 let
-  pico-sdk = pkgs.pico-sdk.override {
-    withSubmodules = true;
-  };
+  local-pico-sdk = pkgs.pico-sdk.overrideDerivation (oldAttrs: {
+    src = pkgs.fetchFromGitHub {
+      owner = "raspberrypi";
+      repo = "pico-sdk";
+      rev = "1.5.1";
+      fetchSubmodules = true;
+      hash = "sha256-GY5jjJzaENL3ftuU5KpEZAmEZgyFRtLwGVg3W1e/4Ho=";
+    };
+  });
+
+  # The following is possible once
+  # https://github.com/NixOS/nixpkgs/pull/321786 is merged
+  # local-pico-sdk = pkgs.pico-sdk.override {
+  #   withSubmodules = true;
+  # };
 in
 pkgs.mkShell {
   buildInputs = with pkgs;
     [
       gcc-arm-embedded
-      pico-sdk
+      local-pico-sdk
       cmake python3   # build requirements for pico-sdk
       udisks          # Interact with bootloader filesystem
       tio             # terminal program to interface with serial
     ];
   shellHook = ''
-   export PICO_SDK_PATH=${pico-sdk}/lib/pico-sdk
+   export PICO_SDK_PATH=${local-pico-sdk}/lib/pico-sdk
   '';
 }
